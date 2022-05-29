f079207c859fd31973c69cbb8ed88daa3198359c
Tomasz Sokół <sokol.tomasz@gmail.com>
2022-05-29 12:27:16 +0200

    eslint

--- feedback_form/src/components/SectionTitle/SectionTitle.jsx
+++ feedback_form/src/components/SectionTitle/SectionTitle.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-function SectionTitle( {title, children} ) {
+function SectionTitle({ title, children }) {
   return (
     <section className="title">
       <h1>{title}</h1>
       {children}
     </section>
-  )
+  );
 }
 
 export default SectionTitle;
